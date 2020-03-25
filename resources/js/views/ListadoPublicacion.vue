ebdd931fbdc917752b351eb2e281b84d378bcafe
germanio7 <german.e.escobar@gmail.com>
2020-03-24 22:39:00 -0300

    listado publicacion casi finalizado

--- resources/js/views/ListadoPublicacion.vue
+++ resources/js/views/ListadoPublicacion.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
+    <div class="bg-gray-300">
         <!-- PAGINAS 8 - 14 - 25 - 56 -->
 
         <div class="flex justify-around items-start lg:px-12">
             <div class="hidden lg:flex">
-                <!-- Desplegable -->
-                <div class="w-64 rounded-lg overflow-hidden shadow-lg">
+                <!-- Insertar Desplegable -->
+                <div class="w-64 bg-white rounded-lg overflow-hidden shadow-lg my-8">
                     <div class="flex justify-between items-center primary lg:secondary">
                         <p class="text-white p-2 pl-4">ORDENAR POR</p>
-                        <!-- Falta icono -->
                         <h1 class="text-white pr-4 lg:hidden">X</h1>
                     </div>
 
@@ -70,64 +69,81 @@
                         >NUEVA PUBLICACIÓN</w-btn>
                     </div>
                 </div>
+                <!-- -------------------- -->
             </div>
-            <div>
-                <div class="container bg-gray-300 m-auto">
+
+            <div class="bg-white rounded-lg overflow-hidden shadow-lg my-8">
+                <div class="container bg-gray-300 lg:bg-white m-auto p-8 lg:px-8">
                     <div class="flex lg:bg-white justify-center lg:justify-start">
-                        <div class="font-bold text-4xl">Listado de Publicaciones</div>
+                        <div class="subtitle bold">Listado de Publicaciones</div>
                     </div>
                 </div>
 
-                <div class="container lg:hidden m-auto p-8">
+                <div class="container lg:hidden m-auto py-4 px-8">
                     <div class="flex">
-                        <w-btn :large="true" :color="$wlinii.secondary">
+                        <w-btn class="flex bold white-text items-center" :color="$wlinii.secondary">
                             FILTRAR
                             <w-icon class="pl-4" icon="bars-white" h="24px"></w-icon>
                         </w-btn>
                     </div>
                 </div>
 
-                <div class="container bg-gray-300 lg:bg-white m-auto p-4 lg:p-2">
-                    <div class="flex-wrap lg:flex justify-around items-center">
+                <div class="container bg-gray-300 lg:bg-white m-auto p-8 lg:pt-0">
+                    <div class="flex-wrap lg:flex justify-between items-center">
                         <div v-for="item in cards" :key="item.i">
                             <div class="my-8 lg:my-4">
-                                <!-- Insert Card -->
+                                <w-card :image="item.image" :hover="'full-hover'">
+                                    <template slot="header">
+                                        <div
+                                            class="flex items-center justify-between caption white-text"
+                                        >
+                                            <div class="flex items-center">
+                                                <div>
+                                                    <div v-if="item.tipo == 'Borrador'">
+                                                        <div class="circleBorrador"></div>
+                                                    </div>
+                                                    <div v-if="item.tipo == 'Vendido'">
+                                                        <div class="circleVendido"></div>
+                                                    </div>
+                                                    <div v-if="item.tipo == 'Publicado'">
+                                                        <div class="circlePublicado"></div>
+                                                    </div>
+                                                </div>
+                                                <p class="px-2">{{item.tipo}}</p>
+                                            </div>
+                                            <div class="flex items-center">
+                                                <w-nav-btn icon="edit" iconH="14px" class="px-4"></w-nav-btn>
+                                                <w-nav-btn
+                                                    icon="parking-solid"
+                                                    iconH="14px"
+                                                    class="px-4"
+                                                ></w-nav-btn>
+                                            </div>
+                                        </div>
+                                    </template>
 
-                                <!-- ----------- -->
-                                <w-card>
-                                    <div class>
-                                        <w-icon
-                                            v-if="item.tipo == 'Borrador'"
-                                            icon="arrow-left"
-                                            h="12px"
-                                            class="p-2"
-                                        ></w-icon>
-                                        <w-icon
-                                            v-if="item.tipo == 'Vendido'"
-                                            icon="arrow-down"
-                                            h="12px"
-                                            class="p-2"
-                                        ></w-icon>
-                                        <w-icon
-                                            v-if="item.tipo == 'Publicacion'"
-                                            icon="youtube-black"
-                                            h="12px"
-                                            class="p-2"
-                                        ></w-icon>
-                                        <p class="p-2">{{item.tipo}}</p>
+                                    <!-- Aca se me complico -->
+                                    <template slot="image">
+                                        <form>
+                                            <w-input></w-input>
+                                            <w-input></w-input>
+                                        </form>
+                                    </template>
+                                    <!-- ----------------- -->
+
+                                    <div>
+                                        <h1 class="caption bold tertiary-text">850 Pennsylvania</h1>
+                                        <p class="caption html">Philiadelphia, Cherry Hill, DC205500</p>
+                                        <p class="caption html">Modificación: 2020/09/05</p>
                                     </div>
-                                    <div class>
-                                        <!-- <w-icon class="p-2" icon="edit" h="14px"></w-icon> -->
-                                        <w-btn icon="parking-solid"></w-btn>
-                                    </div>
-
-                                    <img src="img/departamentos/1.jpg" width="315px" />
-
-                                    <h1 class="font-bold text-tertiary">850 Pennsylvania</h1>
-                                    <p class>Philiadelphia, Cherry Hill, DC205500</p>
-                                    <p>Modificación: 2020/09/05</p>
-                                    <div v-if="item.tipo == 'Publicacion'" class="flex mt-4 w=auto">
-                                        <w-btn :color="$wlinii.primary">CERRAR PUBLICACIÓN</w-btn>
+                                    <div
+                                        v-if="item.tipo == 'Publicado'"
+                                        class="flex justify-center mt-4"
+                                    >
+                                        <w-btn
+                                            class="caption white-text"
+                                            :color="$wlinii.primary"
+                                        >CERRAR PUBLICACIÓN</w-btn>
                                     </div>
                                 </w-card>
                             </div>
@@ -145,16 +161,28 @@ export default {
         return {
             cards: [
                 {
-                    tipo: "Publicacion"
+                    tipo: "Publicado",
+                    image: "/images/departamentos/1.jpg"
                 },
                 {
-                    tipo: "Vendido"
+                    tipo: "Vendido",
+                    image: "/images/departamentos/2.jpg"
                 },
                 {
-                    tipo: "Borrador"
+                    tipo: "Borrador",
+                    image: "/images/departamentos/3.jpg"
                 },
                 {
-                    tipo: "Publicacion"
+                    tipo: "Publicado",
+                    image: "/images/departamentos/4.jpg"
+                },
+                {
+                    tipo: "Borrador",
+                    image: "/images/departamentos/5.jpg"
+                },
+                {
+                    tipo: "Vendido",
+                    image: "/images/departamentos/6.jpg"
                 }
             ]
         };
