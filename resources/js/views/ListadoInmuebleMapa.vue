7fb797b77807c9599895a555e43ee6dca3817355
germanio7 <german.e.escobar@gmail.com>
2020-03-28 02:22:44 -0300

    mapa - comienzo

--- resources/js/views/ListadoInmuebleMapa.vue
+++ resources/js/views/ListadoInmuebleMapa.vue
@@ -1,24 +1,35 @@
 <template>
     <div class="bg-gray-200">
-        <!-- PAGINAS 12 - 19 - 26 - 35 -->
+        <!-- PAGINAS 30 -->
 
         <div class="flex justify-between items-start">
-            <div>
+            <div class="m-8">
                 <filtro-inmueble></filtro-inmueble>
             </div>
 
-            <div>
-                <w-card>
-                    <div class="bg-gray-200 m-auto">
+            <div class="container m-8">
+                <div class=" rounded-lg  shadow-lg">
+                    <div class="bg-gray-200 ">
                         <div
-                            class="flex lg:bg-white items-center justify-center lg:justify-between"
+                            class="flex lg:bg-white items-center justify-center lg:justify-between p-8"
                         >
                             <div class="subtitle bold">
                                 Listado de Inmuebles
                             </div>
                         </div>
                     </div>
-                </w-card>
+
+                    <!-- editar el icono -->
+                    <GoogleMap
+                        :latitude="latitude"
+                        :longitude="longitude"
+                        :apiKey="apiKey"
+                        :zoom="12"
+                        with-marker
+                        :locations="locations"
+                        icon="../components/wlinii_components/iconos/lock.png"
+                    />
+                </div>
             </div>
         </div>
     </div>
@@ -26,10 +37,36 @@
 
 <script>
 import FiltroInmueble from "./filtros/FiltroInmueble.vue";
+import GoogleMap from "vue-maps";
 
 export default {
+    data() {
+        return {
+            latitude: -12.1122095,
+            longitude: -77.047945,
+            apiKey: "AIzaSyAXlj8mkvRRg6KTVesim7xcM8CcceMfcAc",
+            locations: [
+                {
+                    title: "Miraflores",
+                    latitude: -12.1221005,
+                    longitude: -77.0315953
+                },
+                {
+                    title: "San Isidro",
+                    latitude: -12.1001099,
+                    longitude: -77.0554858
+                },
+                {
+                    title: "Santiago de Surco",
+                    latitude: -12.1206998,
+                    longitude: -77.0347257
+                }
+            ]
+        };
+    },
     components: {
-        FiltroInmueble
+        FiltroInmueble,
+        GoogleMap
     }
 };
 </script>
