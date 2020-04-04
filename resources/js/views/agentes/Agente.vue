0f3aa6375f89c5aa178ddfaf82749ecfc672fd28
francodicampli <francodicampli@gmail.com>
2020-04-04 12:35:03 -0300

    Agente almost ready v2

--- resources/js/views/agentes/Agente.vue
+++ resources/js/views/agentes/Agente.vue
@@ -226,36 +226,6 @@
                   <p class="caption">Canceladas</p>
                   <p class="title">{{agent.canceled}}</p>
                 </div>
-
-                <!-- <div class="w-1/2 lg:w-1/4 rounded-lg primary text-white p-5 m-1">
-                  <p class="caption">Acivas</p>
-                  <p class="title">{{agent.active.n}}</p>
-                  <p class="uppercase">
-                    {{agent.active.state}} {{agent.active.stateN}}%
-                    <span class="inline-block">
-                      <svg
-                        :class="agent.active.state=='up' ? 'text-green-500' : ' transform rotate-180 text-red-400 '"
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="fas"
-                        data-icon="arrow-up"
-                        class="h-5 svg-inline--fa fa-arrow-up fa-w-14"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
-                        />
-                      </svg>
-                    </span>
-                  </p>
-                </div>
-                <div class="w-1/2 lg:w-1/4 rounded-lg primary text-white p-5 m-1">
-                  <p class="caption">Canceladas</p>
-                  <p class="title">{{agent.canceled}}</p>
-                </div>-->
               </div>
             </div>
           </div>
@@ -322,9 +292,6 @@
             </div>
           </slide>
         </w-carousel>
-        <div class="flex justify-center my-5">
-          <w-btn :large="true" :dark="true">ver mas propiedades</w-btn>
-        </div>
       </div>
     </div>
     <!-- end carrusel  -->
@@ -388,9 +355,6 @@
             </div>
           </slide>
         </w-carousel>
-        <div class="flex justify-center my-5">
-          <w-btn :large="true" :dark="true">ver mas propiedades</w-btn>
-        </div>
       </div>
     </div>
     <!-- end carrusel  -->
