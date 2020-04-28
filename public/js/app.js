f845a70ecbdaff4c7e42b02479f298d16f22d121
francodicampli <francodicampli@gmail.com>
2020-04-28 20:23:22 -0300

    almost done

--- public/js/app.js
+++ public/js/app.js
@@ -64590,8 +64590,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel-projects\wlinii-laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-projects\wlinii-laravel\resources\css\main.css */"./resources/css/main.css");
+__webpack_require__(/*! C:\xampp\htdocs\prueba\wlinii-laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\prueba\wlinii-laravel\resources\css\main.css */"./resources/css/main.css");
 
 
 /***/ })
