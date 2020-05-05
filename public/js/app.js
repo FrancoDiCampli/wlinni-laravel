0dc3a4c2bf9e4f317e74cf402c54eb4efac0487a
KazmerMaximiliano <kazmermaximiliano@gmail.com>
2020-05-05 10:16:47 -0300

    nothing

--- public/js/app.js
+++ public/js/app.js
@@ -65003,8 +65003,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel-projects\wlinii-laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-projects\wlinii-laravel\resources\css\main.css */"./resources/css/main.css");
+__webpack_require__(/*! C:\xampp\htdocs\projects\wlinii-laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\projects\wlinii-laravel\resources\css\main.css */"./resources/css/main.css");
 
 
 /***/ })
